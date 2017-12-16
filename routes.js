bb5e3bc52f3bc8f3a8841bfe17a6a70195a9fa46
Zed <20992790+zed-apps@users.noreply.github.com>
2017-12-16 00:04:58 -0800

    express router

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ const helmet 		 = require('helmet')
 const express 		 = require('express')
 const router 		 = express.Router()
 
-const HandleError	 = require('./lib/HandleError')
 const improperMethod = require('./routes/http/improperMethod')
+const error404 		 = require('./routes/http/404')
 const validateKey	 = require('./routes/http/validateKey')
 const writes	 	 = require('./routes/http/writes')
 const reads 		 = require('./routes/http/reads')
@@ -16,16 +16,16 @@ module.exports = function(app) {
 	app.use(bodyParser.urlencoded({ extended: true }))
 	app.use(bodyParser.json())
 	app.use(helmet())
-
 	app.get( '/', (req, res) => {
 		res.sendStatus(200)
 	})
+	
 
-	router.get( '/v1/validateKey', (req, res) => {
+	router.get( '/validateKey', (req, res) => {
 		validateKey( req, res )
 	})
 
-	router.use( '/v1/validateKey', ( req, res ) => { 
+	router.use( '/validateKey', ( req, res ) => { 
 		improperMethod( req, res ) 	
 	})
 
@@ -34,26 +34,26 @@ module.exports = function(app) {
 		ALTER DATA
 	*/
 
-	router.post( '/v1/insert', ( req, res ) => { 
+	router.post( '/insert', ( req, res ) => { 
 		writes.insert( req, res ) 	
 	})
 
-	router.use( '/v1/insert', ( req, res ) => { 
+	router.use( '/insert', ( req, res ) => { 
 		improperMethod( req, res, 'POST' ) 	
 	})
 
-	router.put( '/v1/update', ( req, res ) => { 
+	router.put( '/update', ( req, res ) => { 
 		writes.update( req, res ) 	
 	})
 
-	router.use( '/v1/update', ( req, res ) => {  
+	router.use( '/update', ( req, res ) => {  
 		improperMethod( req, res, 'PUT' ) 	
 	})
 
-	router.delete( '/v1/delete/', ( req, res ) => { 
+	router.delete( '/delete/', ( req, res ) => { 
 		writes.delete( req, res ) 	
 	})
-	router.use( '/v1/delete/', ( req, res ) => { 
+	router.use( '/delete/', ( req, res ) => { 
 		improperMethod( req, res, 'DELETE' ) 	
 	})
 	
@@ -62,200 +62,195 @@ module.exports = function(app) {
 
 	// id
 
-	router.get( '/v1/id/:_id', (req, res) => {
+	router.get( '/id/:_id', (req, res) => {
 		reads.readId( req, res ) 
 	})
 
-	router.use( '/v1/id/:_id', ( req, res ) => { 
+	router.use( '/id/:_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/:collection/id/:_id', (req, res) => {
+	router.get( '/:collection/id/:_id', (req, res) => {
 		reads.readId( req, res )
 	})
 
-	router.use( '/v1/:collection/id/:_id', ( req, res ) => { 
+	router.use( '/:collection/id/:_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
 	// count
 
-	router.get( '/v1/count/', (req, res) => {
+	router.get( '/count/', (req, res) => {
 		reads.readCount( req, res )
 	})
 
-	router.get( '/v1/count/:owner_id', (req, res) => {
+	router.get( '/count/:owner_id', (req, res) => {
 		reads.readCount( req, res)
 	})
 
-	router.get( '/v1/:collection/count/', (req, res) => {
+	router.get( '/:collection/count/', (req, res) => {
 		reads.readCount( req, res )
 	})
 
-	router.get( '/v1/:collection/count/:owner_id', (req, res) => {
+	router.get( '/:collection/count/:owner_id', (req, res) => {
 		reads.readCount( req, res  )
 	})
 
-	router.use( '/v1/count/', ( req, res ) => { 
+	router.use( '/count/', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/count/:owner_id', ( req, res ) => { 
+	router.use( '/count/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/count/', ( req, res ) => { 
+	router.use( '/:collection/count/', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/count/:owner_id', ( req, res ) => { 
+	router.use( '/:collection/count/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
 	// hash | hashData
 
-	router.get( '/v1/hash/:hash', (req, res) => {
+	router.get( '/hash/:hash', (req, res) => {
 		reads.readHash( req, res )
 	})
 
-	router.use( '/v1/hash/:hash', (req, res) => {
+	router.use( '/hash/:hash', (req, res) => {
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/hash/:hash/:owner_id', (req, res) => {
+	router.get( '/hash/:hash/:owner_id', (req, res) => {
 		reads.readHash( req, res )
 	})
 
-	router.use( '/v1/hash/:hash/:owner_id', ( req, res ) => { 
+	router.use( '/hash/:hash/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/:collection/hash/:hash', (req, res) => {
+	router.get( '/:collection/hash/:hash', (req, res) => {
 		reads.readHash( req, res )
 	})
 
-	router.use( '/v1/:collection/hash/:hash', ( req, res ) => { 
+	router.use( '/:collection/hash/:hash', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/:collection/hash/:hash/:owner_id', (req, res) => {
+	router.get( '/:collection/hash/:hash/:owner_id', (req, res) => {
 		reads.readHash( req, res )
 	})
 
-	router.use( '/v1/:collection/hash/:hash/:owner_id', ( req, res ) => { 
+	router.use( '/:collection/hash/:hash/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
 	// last | newest | most recent
 
-	router.get( '/v1/last/:owner_id', (req, res) => {
+	router.get( '/last/:owner_id', (req, res) => {
 		reads.readOwner( req, res )
 	})
 
-	router.get( '/v1/last/:owner_id/:limit', (req, res) => {
+	router.get( '/last/:owner_id/:limit', (req, res) => {
 		reads.readOwner( req, res ) 
 	})
 
-	router.get( '/v1/last/:owner_id/:limit/page/:page', (req, res) => {
+	router.get( '/last/:owner_id/:limit/page/:page', (req, res) => {
 		reads.readOwner( req, res ) 
 	})
 
-	router.use( '/v1/last/:owner_id', ( req, res ) => { 
+	router.use( '/last/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/last/:owner_id/:limit', ( req, res ) => { 
+	router.use( '/last/:owner_id/:limit', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/last/:owner_id/:limit/page/:page', ( req, res ) => { 
+	router.use( '/last/:owner_id/:limit/page/:page', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/:collection/last/:owner_id', (req, res) => {
+	router.get( '/:collection/last/:owner_id', (req, res) => {
 		reads.readOwner( req, res )
 	})
 
-	router.get( '/v1/:collection/last/:owner_id/:limit', (req, res) => {
+	router.get( '/:collection/last/:owner_id/:limit', (req, res) => {
 		reads.readOwner( req, res )
 	})
 
-	router.get( '/v1/:collection/last/:owner_id/:limit/page/:page', (req, res) => {
+	router.get( '/:collection/last/:owner_id/:limit/page/:page', (req, res) => {
 		reads.readOwner( req, res ) 
 	})
 	
-	router.use( '/v1/:collection/last/:owner_id', ( req, res ) => { 
+	router.use( '/:collection/last/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/last/:owner_id/:limit', ( req, res ) => { 
+	router.use( '/:collection/last/:owner_id/:limit', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/last/:owner_id/:limit/page/:page', ( req, res ) => { 
+	router.use( '/:collection/last/:owner_id/:limit/page/:page', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 	
 	// first | oldest
 
-	router.get( '/v1/first/:owner_id', (req, res) => {
+	router.get( '/first/:owner_id', (req, res) => {
 		reads.readOwner( req, res, 1 )
 	})
 
-	router.get( '/v1/first/:owner_id/:limit', (req, res) => {
+	router.get( '/first/:owner_id/:limit', (req, res) => {
 		reads.readOwner( req, res, 1 ) 
 	})
 
-	router.get( '/v1/first/:owner_id/:limit/page/:page', (req, res) => {
+	router.get( '/first/:owner_id/:limit/page/:page', (req, res) => {
 		reads.readOwner( req, res, 1 )
 	})
 
-	router.use( '/v1/first/:owner_id', ( req, res ) => { 
+	router.use( '/first/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/first/:owner_id/:limit', ( req, res ) => { 
+	router.use( '/first/:owner_id/:limit', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/first/:owner_id/:limit/page/:page', ( req, res ) => { 
+	router.use( '/first/:owner_id/:limit/page/:page', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.get( '/v1/:collection/first/:owner_id', (req, res) => {
+	router.get( '/:collection/first/:owner_id', (req, res) => {
 		reads.readOwner( req, res, 1 )
 	})
 
-	router.get( '/v1/:collection/first/:owner_id/:limit', (req, res) => {
+	router.get( '/:collection/first/:owner_id/:limit', (req, res) => {
 		reads.readOwner( req, res, 1 )
 	})
 
-	router.get( '/v1/:collection/first/:owner_id/:limit/page/:page', (req, res) => {
+	router.get( '/:collection/first/:owner_id/:limit/page/:page', (req, res) => {
 		reads.readOwner( req, res, 1 )
 	})
 	
-	router.use( '/v1/:collection/first/:owner_id', ( req, res ) => { 
+	router.use( '/:collection/first/:owner_id', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/first/:owner_id/:limit', ( req, res ) => { 
+	router.use( '/:collection/first/:owner_id/:limit', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
-	router.use( '/v1/:collection/first/:owner_id/:limit/page/:page', ( req, res ) => { 
+	router.use( '/:collection/first/:owner_id/:limit/page/:page', ( req, res ) => { 
 		improperMethod( req, res ) 
 	})
 
 	// 404 everything else 
 	
 	router.use(( req, res ) => { 
-		res.setHeader( 'Content-Type', 'application/vnd.api+json' )
-		let apiHandler = req.router.get( 'apiHandler' )
-		let apiKey = apiHandler.parseApiKey( req.headers.authorization )	
-		let err = HandleError( '[API] Path does not exist', false, 404 )
-		apiHandler.logRequest( err.code, err.code, apiKey, err.msg )
-		res.status(err.code).send(err.msg)
+		error404( req, res )
 	})
 
-	app.use( "/", router )
+	return router
 }
